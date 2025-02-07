e3f922269107783080d3294cfcd805c1f9a1ae4e
Giovanni Valente <giovannivalente237@gmail.com>
2025-02-07 18:27:20 -0300

    Exercício Vetores

---
+++ App.java
@@ -0,0 +1,55 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+       
+        Locale.setDefault(Locale.US);
+        Scanner scn = new Scanner(System.in);
+       
+
+        System.out.print("Quantas pessoas serao digitadas? ");
+        int n = scn.nextInt();
+
+        String[] nomes = new String[n];
+        int[] idades = new int[n];
+        double[] alturas = new double[n];
+
+        for(int i = 0; i < n; i++) {
+            System.out.println("Dados da " + (i+1) + "a pessoa");
+            System.out.print("Nome: ");
+            nomes[i] = scn.next();
+            System.out.print("Idade: ");
+            idades[i] = scn.nextInt();
+            System.out.print("Altura: ");
+            alturas[i] = scn.nextDouble();
+        }
+
+        double soma = 0;
+        for(int i = 0; i < n; i++) {
+            soma = soma + alturas[i];
+        } 
+        double mediaAlturas = soma / n;
+
+        System.out.println();
+        System.out.printf("Altura média: %.2f%n", mediaAlturas);
+
+        int cont = 0;
+        for(int i = 0; i < n; i++) {
+            if (idades[i] < 16) {
+                cont = cont + 1;
+            }
+        }
+
+        double percent = cont * 100.0 / n;
+        System.out.printf("Pessoas com menos de 16 anos: %.1f%%%n", percent);
+        
+        for(int i = 0; i < n; i++) {
+            if (idades[i] < 16) {
+               System.out.println(nomes[i]); 
+            }
+        }
+
+        scn.close();
+    }
+}
